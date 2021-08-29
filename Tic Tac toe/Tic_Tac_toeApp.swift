59f14748e0ac67425ad552a505294cc7699152be
matalee <6110613111@student.tu.ac.th>
2021-08-29 22:40:51 +0700

    finish

---
+++ Tic Tac toe/Tic_Tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tic_Tac_toeApp.swift
+//  Tic Tac toe
+//
+//  Created by Cheunchanok Phewkleang on 29/8/2564 BE.
+//
+
+import SwiftUI
+
+@main
+struct Tic_Tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
